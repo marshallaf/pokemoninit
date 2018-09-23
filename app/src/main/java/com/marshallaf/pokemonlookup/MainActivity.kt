036bd3840f276af28025728d3c5fff584d3cc9ca
Andrew Marshall <marshall.a.f@gmail.com>
2018-09-23 11:11:05 -0500

    Add number entry fragment

--- app/src/main/java/com/marshallaf/pokemonlookup/MainActivity.kt
+++ app/src/main/java/com/marshallaf/pokemonlookup/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.marshallaf.pokemonlookup
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    if (savedInstanceState == null) {
+      val fragmentManager = supportFragmentManager
+      fragmentManager.beginTransaction()
+          .replace(android.R.id.content, NumberEntryFragment())
+          .commit()
+    }
   }
 }
