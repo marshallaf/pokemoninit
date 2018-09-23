f60ff218e486b8ca8471d3b5c6eb6b985fda0c71
Andrew Marshall <marshall.a.f@gmail.com>
2018-09-23 11:37:24 -0500

    Restructure packages

--- app/src/main/java/com/marshallaf/pokemonlookup/ui/MainActivity.kt
+++ app/src/main/java/com/marshallaf/pokemonlookup/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.marshallaf.pokemonlookup
+package com.marshallaf.pokemonlookup.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
--- app/src/main/java/com/marshallaf/pokemonlookup/ui/NumberEntryFragment.kt
+++ app/src/main/java/com/marshallaf/pokemonlookup/ui/NumberEntryFragment.kt
@@ -1,10 +1,11 @@
-package com.marshallaf.pokemonlookup
+package com.marshallaf.pokemonlookup.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.marshallaf.pokemonlookup.R
 
 class NumberEntryFragment : Fragment() {
 
