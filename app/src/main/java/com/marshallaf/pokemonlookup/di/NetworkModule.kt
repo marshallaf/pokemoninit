d1a06939a37c3484e9da73ea58338658ca1b00e8
Andrew Marshall <marshall.a.f@gmail.com>
2018-10-04 22:09:50 -0500

    Finish connecting to network

--- app/src/main/java/com/marshallaf/pokemonlookup/di/NetworkModule.kt
+++ app/src/main/java/com/marshallaf/pokemonlookup/di/NetworkModule.kt
@@ -4,7 +4,9 @@ import com.marshallaf.pokemonlookup.network.PokemonApi
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
+import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -16,7 +18,12 @@ class NetworkModule {
   @Provides
   @Singleton
   fun providesOkHttpClient(): OkHttpClient {
-    return OkHttpClient()
+    return OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().apply {
+              level = HttpLoggingInterceptor.Level.BASIC
+            })
+        .build()
   }
 
   @Provides
@@ -30,7 +37,7 @@ class NetworkModule {
   fun providesRetrofit(okHttp: OkHttpClient, moshi: Moshi): Retrofit {
     return Retrofit.Builder()
         .baseUrl("https://pokeapi.co/api/v2/")
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .client(okHttp)
         .build()
